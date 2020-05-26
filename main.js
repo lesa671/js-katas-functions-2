f23238dcdd97b6603ff2bf7a61a4a1169e74c25a
Lesa Ducksworth <lducks67@gmail.com>
2020-05-26 17:33:55 -0500

    changed javascript for factorial kata to not use an array

--- main.js
+++ main.js
@@ -51,35 +51,64 @@ console.log(subtractNum(4, 5))
 
 
 
-function getFactorial(n) {
-    let factorBase = n
-    let factorStep //temporarily holds each multiplication result of the factorial to be added together
-    let factorArray = []
-    let factorArrayTotal
+// function getFactorial(n) {
+// let factorBase = n
+// let factorStep //temporarily holds each multiplication result of the factorial to be added together
+// let factorArray = []
+// let factorArrayTotal
 
 
-    for (let index = 1; index < n; index++) {
+// for (let index = 1; index < n; index++) {
 
-        let factorMultiplier = subtractNum(factorBase, 1)
-        factorStep = multiplyNum(n, factorMultiplier)
-        // console.log(factorStep)
+//     let factorMultiplier = subtractNum(factorBase, 1)
+//     factorStep = multiplyNum(n, factorMultiplier)
+//     // console.log(factorStep)
 
-        factorArray.push(factorStep)
-        // console.log(factorArray)
+//     factorArray.push(factorStep)
+//     // console.log(factorArray)
 
-        factorBase--
+//     factorBase--
 
-    }
+// }
 
-    factorArrayTotal = factorArray.reduce(addNum, 0)
-    // console.log(factorArrayTotal)
+// factorArrayTotal = factorArray.reduce(addNum, 0)
+// // console.log(factorArrayTotal)
 
-    return factorArrayTotal
+//     return factorArrayTotal
 
-}
-console.log(getFactorial(5))
+// }
+// console.log(getFactorial(5))
 
 // if can't use array in kata 4, then need to put another for loop or if statement to add the factorStep values to get the final result?
+
+
+function getFactorial(n) {
+    let factorBase = n
+    let factorResult
+    let factorMultiplier1 = n
+
+    for (let index = 1; index < (n); index++) {
+        
+        let factorMultiplier2 = subtractNum(factorBase, 1)
+        factorResult = multiplyNum(factorMultiplier1, factorMultiplier2)
+        console.log(factorResult)
+
+        factorBase-- 
+
+        factorMultiplier1 = factorResult
+        console.log(factorMultiplier1)
+        
+    }
+    return factorResult
+
+}
+
+console.log(getFactorial(7))
+
+
+
+
+
 
 
 // kata 5 Fibonacci
@@ -90,15 +119,15 @@ function Fibonacci(n) {
 
     for (let index = 2; index < n; index++) {
         arrayFib[index] = addNum(arrayFib[subtractNum(index, 2)], arrayFib[subtractNum(index, 1)])
-        // console.log(arrayFib[index])
-        // console.log(arrayFib)
-    }
+        console.log(arrayFib[index])
+        console.log(arrayFib)
+    } //for loop to generate arrayFib up to nth number
 
-    index = subtractNum(arrayFib.length, 1)
-    // console.log(index)
+    index = subtractNum(arrayFib.length, 1) //to get the index position of the nth number
+    console.log(index)
 
     return arrayFib[index]
 
 }
 
-console.log(Fibonacci(10))
+console.log(Fibonacci(15))
